a36cbb925126615338faa21a81bc97d8e3f5832b
AbduGad <abdo79gad@gmail.com>
2024-08-07 13:10:32 +0300

    "Added controllers, routes, and server setup for API endpoints"

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,1 +1,17 @@
-#!/usr/bin/node
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+class AppController {
+  static getStatus(request, response) {
+    const redisstatus = redisClient.isAlive();
+    const dbstatus = dbClient.isAlive();
+    response.status(200).send({ redis: redisstatus, db: dbstatus });
+  }
+
+  static async getStats(request, response) {
+    const userdocumentsnum = await dbClient.nbUsers();
+    const filesdocumentsnum = await dbClient.nbFiles();
+    response.status(200).send({ users: userdocumentsnum, files: filesdocumentsnum });
+  }
+}
+module.exports = AppController;
